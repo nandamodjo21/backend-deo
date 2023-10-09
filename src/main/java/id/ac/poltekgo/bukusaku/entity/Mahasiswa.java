cf92d658e03d3bc03b920ce21e981f9f9558d7b1
nandamodjo21 <jefrandamodjo.mhs20@ti.poligon.ac.id>
2023-10-09 22:59:50 +0800

    fix: update mahasiswa

--- src/main/java/id/ac/poltekgo/bukusaku/entity/Mahasiswa.java
+++ src/main/java/id/ac/poltekgo/bukusaku/entity/Mahasiswa.java
@@ -1,42 +1,54 @@
-package id.ac.poltekgo.bukusaku.entity;
+// package id.ac.poltekgo.bukusaku.entity;
 
 // import org.hibernate.annotations.GenericGenerator;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+// // import org.hibernate.annotations.GenericGenerator;
 
-@Entity
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Table(name = "t_mahasiswa")
-public class Mahasiswa {
+// import jakarta.persistence.Column;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.JoinColumn;
+// import jakarta.persistence.ManyToOne;
+// // import jakarta.persistence.PrePersist;
+// import jakarta.persistence.Table;
+// import lombok.AllArgsConstructor;
+// import lombok.Getter;
+// import lombok.NoArgsConstructor;
+// import lombok.Setter;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(name = "nim", columnDefinition = "VARCHAR(255)")
-    private String id;
+// @Entity
+// @Getter
+// @Setter
+// @AllArgsConstructor
+// @NoArgsConstructor
+// @Table(name = "t_mahasiswa")
+// public class Mahasiswa {
 
-    @ManyToOne
-    @JoinColumn(name = "id_biodata")
-    private Biodata nik;
+// @Id
+// @GeneratedValue(generator = "uuid2")
+// @GenericGenerator(name = "uuid2", strategy =
+// "org.hibernate.id.UUIDGenerator")
+// @Column(name = "idmhs", columnDefinition = "VARCHAR(255)")
+// private String id;
 
-    @ManyToOne
-    @JoinColumn(name = "id_prodi")
-    private Prodi prodi;
+// @ManyToOne
+// @JoinColumn(name = "id_biodata")
+// private User idUser;
 
-    @Column(name = "status_mhs")
-    private Integer statusMhs;
+// @ManyToOne
+// @JoinColumn(name = "id_prodi")
+// private Prodi prodi;
 
-}
+// @Column(name = "status_mhs")
+// private Integer statusMhs = 2;
+
+// // @PrePersist
+// // public void setDefaultStatus() {
+// // if (statusMhs == null) {
+// // statusMhs = 1;
+// // }
+// // }
+
+// }
