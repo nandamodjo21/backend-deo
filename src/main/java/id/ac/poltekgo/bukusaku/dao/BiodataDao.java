24048acf62c033abd1cfc0346e011ad05ccc96d8
nandamodjo21 <jefrandamodjo.mhs20@ti.poligon.ac.id>
2023-10-09 23:00:06 +0800

    initial

---
+++ src/main/java/id/ac/poltekgo/bukusaku/dao/BiodataDao.java
@@ -0,0 +1,9 @@
+// package id.ac.poltekgo.bukusaku.dao;
+
+// import org.springframework.data.jpa.repository.JpaRepository;
+
+// import id.ac.poltekgo.bukusaku.entity.Biodata;
+
+// public interface BiodataDao extends JpaRepository<Biodata, String> {
+
+// }
---
+++ src/main/java/id/ac/poltekgo/bukusaku/dao/MahasiswaDao.java
@@ -0,0 +1,9 @@
+// package id.ac.poltekgo.bukusaku.dao;
+
+// import org.springframework.data.jpa.repository.JpaRepository;
+
+// import id.ac.poltekgo.bukusaku.entity.Mahasiswa;
+
+// public interface MahasiswaDao extends JpaRepository<Mahasiswa, String> {
+
+// }
